79ea5f9d508407f017cccf813422b5165e5f5cf3
jordandevaney <jordandevaney28@gmail.com>
2023-07-01 13:40:48 -0400

    create migration for database

---
+++ server/data/migrations/20230701172120_cleaning.js
@@ -0,0 +1,75 @@
+exports.up = function (knex) {
+	return knex.schema
+		.createTable("employee", (tbl) => {
+			tbl.increments();
+			tbl.text("name").notNullable();
+			tbl.text("email").notNullable();
+			tbl.text("phone").notNullable();
+			tbl.text("username").notNullable();
+			tbl.text("password").notNullable();
+			tbl.float("hourly_pay").notNullable();
+		})
+		.createTable("location", (tbl) => {
+			tbl.increments();
+			tbl.text("name").notNullable();
+			tbl.text("address").notNullable();
+			tbl.text("phone").notNullable();
+			tbl.text("username").notNullable();
+			tbl.text("password").notNullable();
+		})
+		.createTable("product", (tbl) => {
+			tbl.increments();
+			tbl.text("name").notNullable();
+			tbl.integer("quantity");
+			tbl.text("use").notNullable();
+			tbl.text("status");
+			tbl.text("type");
+		})
+		.createTable("employee_locations", (tbl) => {
+			tbl
+				.integer("employee_id")
+				.notNullable()
+				.unsigned()
+				.references("id")
+				.inTable("employee")
+				.onUpdate("CASCADE")
+				.onDelete("CASCADE");
+			tbl
+				.integer("location_id")
+				.notNullable()
+				.unsigned()
+				.references("id")
+				.inTable("location")
+				.onUpdate("CASCADE")
+				.onDelete("CASCADE");
+			tbl.primary(["employee_id", "location_id"]);
+		})
+		.createTable("product_locations", (tbl) => {
+			tbl
+				.integer("product_id")
+				.notNullable()
+				.unsigned()
+				.references("id")
+				.inTable("product")
+				.onUpdate("CASCADE")
+				.onDelete("CASCADE");
+			tbl
+				.integer("location_id")
+				.notNullable()
+				.unsigned()
+				.references("id")
+				.inTable("location")
+				.onUpdate("CASCADE")
+				.onDelete("CASCADE");
+			tbl.primary(["product_id", "location_id"]);
+		});
+};
+
+exports.down = function (knex) {
+	return knex.schema
+		.dropTableIfExist("product_locations")
+		.dropTableIfExist("employee_locations")
+		.dropTableIfExist("product")
+		.dropTableIfExist("location")
+		.dropTableIfExist("employee");
+};
---
+++ server/knexfile.js
@@ -0,0 +1,30 @@
+module.exports = {
+	development: {
+		client: "pg",
+		debug: true,
+		connection: {
+			host: process.env.HOST,
+			port: process.env.DB_PORT,
+			database: "cleaning",
+			user: process.env.USER,
+			password: process.env.PASSWORD,
+		},
+		migrations: {
+			directory: "./data/migrations",
+		},
+		ssl: true,
+		useNullAsDefault: true,
+	},
+
+	production: {
+		client: "postgresql",
+		debug: true,
+		connection: process.env.DATABASE_URL,
+		migrations: {
+			directory: "./data/migrations",
+		},
+		ssl: {
+			rejectUnauthorized: false,
+		},
+	},
+};
